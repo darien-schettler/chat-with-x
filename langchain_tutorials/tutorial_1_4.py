584820603f83b9fd1b0cd1673de012caf69a99b7
Darien Schettler <darien_schettler@hotmail.com>
2023-04-01 19:41:48 -0400

    Finish tutorial 4

--- langchain_tutorials/tutorial_1_4.py
+++ langchain_tutorials/tutorial_1_4.py
@@ -1,10 +1,23 @@
 import argparse
 from langchain import OpenAI, ConversationChain
-from misc.utils import retrieve_local_api_keys
-from colorama import init, Fore
+from misc.utils import retrieve_local_api_keys, print_colored_output
 
 
-def conversation_script(conversation_inputs, model_name="text-davinci-003", temperature=0, verbose=True):
+def conversation_script(model_name="text-davinci-003", temperature=0, verbose=True,
+                        input_clr="blue", response_clr="green", **kwargs):
+    """ Run a conversation using a LangChain ConversationChain
+
+    Args:
+        model_name (str): Model name for the LLM
+        temperature (float): Temperature for randomness in output
+        verbose (bool): Verbose output
+        input_clr (str): Color to use for input text label
+        response_clr (str): Color to use for response text label
+        **kwargs: Additional keyword arguments
+
+    Returns:
+        None; prints the response to the query
+    """
     # Load the API keys from the .env file
     retrieve_local_api_keys()
 
@@ -14,24 +27,25 @@ def conversation_script(conversation_inputs, model_name="text-davinci-003", temp
     # Initialize the ConversationChain
     conversation = ConversationChain(llm=llm, verbose=verbose)
 
-    # Iterate over conversation inputs and run the ConversationChain
-    for i, input_text in enumerate(conversation_inputs):
+    # Continue the conversation loop until the user decides to exit
+    while True:
+        input_text = input("\nEnter your message (or press enter to exit):\n")
+        if not input_text: break
         response = conversation.predict(input=input_text)
-        print(f"{Fore.BLUE}Input {i + 1:2d}         :{Fore.RESET} '{input_text}'")
-        print(f"{Fore.GREEN}Response {i + 1:2d}      :{Fore.RESET} '{response.strip()}'\n")
+        print_colored_output(input_text, response, input_clr, response_clr)
 
 
 def main():
     parser = argparse.ArgumentParser(description="Run a conversation using a LangChain ConversationChain")
-    parser.add_argument("conversation_inputs", nargs="+", type=str,
-                        help="List of conversation inputs to be processed by the ConversationChain")
     parser.add_argument("-n", "--model_name", type=str, default="text-davinci-003", help="Model name for the LLM")
     parser.add_argument("-t", "--temperature", type=float, default=0, help="Temperature for randomness in output")
     parser.add_argument("-v", "--verbose", action="store_true", help="Verbose output")
+    parser.add_argument("--input_clr", type=str, default="blue", help="Color to use for input text label")
+    parser.add_argument("--response_clr", type=str, default="green", help="Color to use for response text label")
     args = parser.parse_args()
 
     # Run the conversation script with the provided arguments
-    conversation_script(args.conversation_inputs, args.model_name, args.temperature, args.verbose)
+    conversation_script(**args.__dict__)
 
 
 if __name__ == "__main__":
